2db054555889290ba3c04e395491d21ae14d2321
Elijah Chong <dissaryy@gmail.com>
2026-06-10 19:24:57 +0800

    Added Favicon, minimal bootstrap, h1/web title

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-
+import { Container } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 
 export default function App() {
   
 
   return (
-    
+    <Container>
+      <h1 className='m-3'>Gym Hood</h1>
+    </Container>
   )
 }
